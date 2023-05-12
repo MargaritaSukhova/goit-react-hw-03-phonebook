28e82ca01109bbaad7b3797dc9c259d39e875f40
MargaritaSukhova <114304966+MargaritaSukhova@users.noreply.github.com>
2023-05-12 15:43:23 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
+import { Container, Title, SubTitle} from '../App/App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleFormSubmit = ({ name, number }) => {
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (this.checkContacts(name)) {
+      return alert(`${name} is already in contacts list`);
+    }
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  checkContacts = (name) => {
+    const normalizedName = name.toLocaleLowerCase();
+    return this.state.contacts.find(contact => contact.name.toLocaleLowerCase() === normalizedName)
+  }
+
+  handleFilerChange = (e) => {
+      this.setState({
+      filter: e.currentTarget.value,
+    });
+  }
+
+  handleContactDelete = (contactId) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+
+  getVisibleContacts = () => {
+const normalizedFilter = this.state.filter.toLocaleLowerCase();
+   return this.state.contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter))
+  }
+  
+
+  render() {
+
+    const visibleContacs = this.getVisibleContacts();
+    console.log(visibleContacs);
+
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.handleFormSubmit} />
+        <SubTitle>Contacts</SubTitle>
+        <Filter value={this.state.filter} onChangeFilter={this.handleFilerChange} />
+        <ContactList contacts={visibleContacs} onContactDelete={this.handleContactDelete} />
+      </Container>
+    );
+  }
+}
+
+export default App;
