d47a0d397ea158f77a8c8e7827c4ba8cf48dfbfb
MargaritaSukhova <margarytasu@gmail.com>
2023-05-16 17:11:18 +0300

    prettier

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
-import { Container, Title, SubTitle} from '../App/App.styled';
+import { Container, Title, SubTitle } from '../App/App.styled';
 
 class App extends Component {
   state = {
@@ -11,8 +11,19 @@ class App extends Component {
     filter: '',
   };
 
-  handleFormSubmit = ({ name, number }) => {
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
 
+    if (contacts) this.setState({ contacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contatcs) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleFormSubmit = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
@@ -28,41 +39,33 @@ class App extends Component {
     }));
   };
 
-  checkContacts = (name) => {
+  checkContacts = name => {
     const normalizedName = name.toLocaleLowerCase();
-    return this.state.contacts.find(contact => contact.name.toLocaleLowerCase() === normalizedName)
-  }
+    return this.state.contacts.find(
+      contact => contact.name.toLocaleLowerCase() === normalizedName
+    );
+  };
 
-  handleFilerChange = (e) => {
-      this.setState({
+  handleFilerChange = e => {
+    this.setState({
       filter: e.currentTarget.value,
     });
-  }
+  };
 
-  handleContactDelete = (contactId) => {
+  handleContactDelete = contactId => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-    }))
-  }
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
   getVisibleContacts = () => {
-const normalizedFilter = this.state.filter.toLocaleLowerCase();
-   return this.state.contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter))
-  }
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    const normalizedFilter = this.state.filter.toLocaleLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
 
-    if (contacts)  this.setState({ contacts })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contatcs) {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-  }
-  }
-  
   render() {
-
     const visibleContacs = this.getVisibleContacts();
 
     return (
@@ -70,8 +73,14 @@ const normalizedFilter = this.state.filter.toLocaleLowerCase();
         <Title>Phonebook</Title>
         <ContactForm onSubmit={this.handleFormSubmit} />
         <SubTitle>Contacts</SubTitle>
-        <Filter value={this.state.filter} onChangeFilter={this.handleFilerChange} />
-        <ContactList contacts={visibleContacs} onContactDelete={this.handleContactDelete} />
+        <Filter
+          value={this.state.filter}
+          onChangeFilter={this.handleFilerChange}
+        />
+        <ContactList
+          contacts={visibleContacs}
+          onContactDelete={this.handleContactDelete}
+        />
       </Container>
     );
   }
